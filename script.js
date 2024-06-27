400b3e782adee92cfb3fc540fdc72dd69220a04d
Junaid Saifi <136296659+610Junaid@users.noreply.github.com>
2024-06-27 15:46:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("fa-xmark");
+  navbar.classList.toggle("active");
+};
+
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offset - 150;
+    let hight = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + hight) {
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  menuIcon.classList.remove("fa-xmark");
+  navbar.classList.remove("active");
+};
+
+ScrollReveal({
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".my-image, .service-container, .project-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-image", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
+const typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Web Designer"],
+  typeSpeed: 40,
+  backSpeed: 70,
+  backDelay: 1000,
+  loop: true,
+});
